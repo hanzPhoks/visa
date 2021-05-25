95234ea019ba1960725360eba544c07625561f06
Jeanno <hanzPhoks@users.noreply.github.com>
2021-05-25 17:53:10 +0200

    added start and bottom map

---
+++ tsx/retro_interior_atlas_32x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="retro_interior_atlas_32x" tilewidth="32" tileheight="32" tilecount="768" columns="48">
+ <image source="../retro_interior_atlas_32x.png" width="1536" height="512"/>
+</tileset>

dada8bd1556ff00d60a15a2b75900fdceb743225
Jeanno <hanzPhoks@users.noreply.github.com>
2021-05-25 18:09:29 +0200

    rm tsx

--- tsx/logos.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="logos" tilewidth="32" tileheight="32" tilecount="400" columns="40">
- <image source="../logos.png" width="1280" height="320"/>
-</tileset>
